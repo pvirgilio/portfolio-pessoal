8d2bcda94e9bf5bfb7c8884abea8ceb2f083d7ea
Pedro Virgilio <pvvirg16@gmail.com>
2023-07-03 20:24:25 -0300

    Mudança de menu tablet e desktop

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,55 @@
-const btnMenu = document.getElementById('btnAbrir');
-const menu = document.getElementById('ctnMenu');
+// const btnMenu = document.getElementById('btnAbrir');
+// const menu = document.getElementById('ctnMenu');
 
 
-btnMenu.addEventListener('click', function() {
-  if (menu.classList.contains('menu-aberto')) {
-    menu.classList.remove('menu-aberto')
-    btnMenu.classList.remove('fa-xmark')
-    btnMenu.classList.add('fa-bars')
-  } else {
-    menu.classList.add('menu-aberto')
-    btnMenu.classList.remove('fa-bars')
-    btnMenu.classList.add('fa-xmark')
+// btnMenu.addEventListener('click', function() {
+//   if (menu.classList.contains('menu-aberto')) {
+//     menu.classList.remove('menu-aberto')
+//     btnMenu.classList.remove('fa-xmark')
+//     btnMenu.classList.add('fa-bars')
+//   } else {
+//     menu.classList.add('menu-aberto')
+//     btnMenu.classList.remove('fa-bars')
+
+//     btnMenu.classList.add('fa-xmark')
 
    
+//   }
+// });
+
+
+const mediaQuery = window.matchMedia('(max-width: 767px)');
+const btnMenu = document.querySelector('#btnAbrir');
+const menu = document.querySelector('#menu');
+
+const mediaQueryHandler = function (event) {
+  if (event.matches) {
+    // A consulta de mídia é atendida (largura da janela é menor ou igual a 768px)
+    btnMenu.addEventListener('click', toggleMenu);
+  } else {
+    // A consulta de mídia não é atendida (largura da janela é maior que 768px)
+    btnMenu.removeEventListener('click', toggleMenu);
+    menu.classList.remove('menu-aberto');
+    btnMenu.classList.remove('fa-xmark');
+    btnMenu.classList.add('fa-bars');
   }
-});
+};
+
+const toggleMenu = function() {
+  if (menu.classList.contains('menu-aberto')) {
+    menu.classList.remove('menu-aberto');
+    btnMenu.classList.remove('fa-xmark');
+    btnMenu.classList.add('fa-bars');
+  } else {
+    menu.classList.add('menu-aberto');
+    btnMenu.classList.remove('fa-bars');
+    btnMenu.classList.add('fa-xmark');
+  }
+};
+
+mediaQuery.addListener(mediaQueryHandler);
+mediaQueryHandler(mediaQuery);
+
 
 
 // const btnMenu = document.getElementById('btnMenu')
